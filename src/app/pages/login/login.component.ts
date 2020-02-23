a6f2a51d9f4926785ebbb9a60a3e04c7b7d3a7f5
Caleb Isaac Mora Díaz <calexico659@gmail.com>
2020-02-23 18:54:33 -0500

    SweetAlert2 Modal Loading en Login y Registro

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +15,7 @@ export class LoginComponent implements OnInit {
 
   public usuario: UsuarioModel;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,  private router: Router) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -20,10 +23,23 @@ export class LoginComponent implements OnInit {
 
   public login(form: NgForm) {
     if (form.invalid) { return; }
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere, por favor...'
+    });
+    Swal.showLoading();
     this.authService.login(this.usuario).subscribe(response => {
       console.log(response);
+      Swal.close();
+      this.router.navigateByUrl('/home');
     }, log => {
       console.error(log.error.error.message);
+      Swal.fire({
+        title: 'Error al autenticar',
+        icon: 'error',
+        text: log.error.error.message
+      });
     });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,28 +1,52 @@
 import { Injectable } from '@angular/core';
 import { UsuarioModel } from '../models/usuario.model';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
   private url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
   private key = 'AIzaSyCzATluNUQrD-c8Hn8tVxZTtqH-Qh6JJ8I';
-  // Crear usuarios
-  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
-  // Login
-  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+  public token: string;
 
   constructor(private http: HttpClient) { }
 
   logout() { }
 
   login(usuario: UsuarioModel) {
-    return this.http.post(`${this.url}signInWithPassword?key=${this.key}`, {...usuario, returnSecureToken: true});
+    return this.http.post(`${this.url}signInWithPassword?key=${this.key}`, {...usuario, returnSecureToken: true})
+    .pipe(map(resp => {
+      // tslint:disable-next-line: no-string-literal
+      this.saveToken( resp['idToken']);
+      return resp;
+    }));
   }
 
   newUser(usuario: UsuarioModel) {
-    return this.http.post(`${this.url}signUp?key=${this.key}`, {...usuario, returnSecureToken: true});
+    return this.http.post(`${this.url}signUp?key=${this.key}`, {...usuario, returnSecureToken: true})
+      .pipe(map(resp => {
+        // tslint:disable-next-line: no-string-literal
+        this.saveToken( resp['idToken']);
+        return resp;
+      }));
+  }
+
+  private saveToken(idToken: string) {
+    this.token = idToken;
+    localStorage.setItem('token', idToken);
+  }
+
+  public readToken() {
+    if (localStorage.getItem('token')) {
+      this.token = localStorage.getItem('token');
+    } else {
+      this.token = '';
+    }
+    return this.token;
   }
 
 }
